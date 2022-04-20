619c6a4f70003f4055e5022eda5369804e53583c
r4yns2 <89570237+r4yns2@users.noreply.github.com>
2022-04-20 11:13:03 +0200

    første commit med oppsett.

---
+++ src/test/java/com/example/tripsleep/TripsleepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripsleep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsleepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
