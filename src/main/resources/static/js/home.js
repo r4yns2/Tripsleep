0164d5cbaa0e99f32b6cc925ec8dcc94b4a3d3f4
r4yns2 <89570237+r4yns2@users.noreply.github.com>
2022-05-28 19:17:26 +0200

    Leveringsklar oppgave.

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -13,27 +13,4 @@ roomsContainers.forEach((item, i) => {
     preBtn[i].addEventListener('click', () => {
         item.scrollLeft -= containerWidth;
     })
-})
-
-//Search button click
-
-document.getElementById("filter-btn").addEventListener("click", () => {
-    //initializations
-    let searchInput = document.getElementById("search-input").value;
-    window.location.href = "/Tripsleep/static/rooms/rooms.html";
-
-
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".card");
-    //loop through all elements
-    elements.forEach((element, index) => {
-        //check if text includes the search value
-        if (element.innerText.includes(searchInput.toUpperCase())) {
-            //display matching card
-            cards[index].classList.remove("hide");
-        } else {
-            //hide others
-            cards[index].classList.add("hide");
-            }
-        });
 });
